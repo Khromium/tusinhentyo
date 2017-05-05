8d6e98d966aa9017f637af1388b0a209a53d86bc
khrom <devs@outlook.jp>
2017-05-05 21:19:14 +0900

    add: qpskを追加

--- src/as/Main.java
+++ src/as/Main.java
@@ -27,14 +27,14 @@ public class Main extends JFrame {
     private static double BIT_NUM = Math.pow(10, 7);
     private static int FS = 100;//sampling freq
     private static int FC = 50; //carrier freq
-    private static int AMP = 1;
-    private static double SK = AMP * Math.cos(2 * Math.PI * FC * 0 + 1 * Math.PI);
+    private static double BPSK = 1 * Math.cos(2 * Math.PI * FC * 0 + (1) * Math.PI);
+    private static double QPSK = Math.sqrt(2) * Math.cos(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);
 
 
     public static void main(String[] args) {
         Main main = new Main();
         long start = System.currentTimeMillis();
-
+//
         List<CalBPSK> calBPSKS = new ArrayList<>();
         for (int i = 0; i < SN_RANGE; i++) {
             calBPSKS.add(new CalBPSK(i));
@@ -47,47 +47,61 @@ public class Main extends JFrame {
                 e.printStackTrace();
             }
         });
-//        List<CalQPSK> calBPSKS = new ArrayList<>();
-//        for (int i = 0; i < SN_RANGE; i++) {
-//            calBPSKS.add(new CalQPSK(i));
-//            calBPSKS.get(i).start();
-//        }
-//        calBPSKS.forEach(s -> {
-//            try {
-//                s.join();
-//            } catch (InterruptedException e) {
-//                e.printStackTrace();
-//            }
-//        });
 
         List<Double> dataList = new ArrayList<>();
         calBPSKS.forEach(s -> dataList.add(s.res));
 
-        main.createChart(dataList);
-        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        main.setBounds(10, 10, 500, 500);
-        main.setTitle("にゃあ");
-        main.setVisible(true);
+        main.createChart(dataList, "bpsk");
+//        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        main.setBounds(10, 10, 500, 500);
+//        main.setTitle("にゃあ");
+//        main.setVisible(true);
         long end = System.currentTimeMillis();
+        System.out.println("time" + (end - start) + "ms");
+
+//        //qpsk
+        start = System.currentTimeMillis();
+
+        List<CalQPSK> calQPSKS = new ArrayList<>();
+        for (int i = 0; i < SN_RANGE; i++) {
+            calQPSKS.add(new CalQPSK(i));
+            calQPSKS.get(i).start();
+        }
+        calQPSKS.forEach(s -> {
+            try {
+                s.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+
+        List<Double> dataLists = new ArrayList<>();
+        calQPSKS.forEach(s -> dataLists.add(s.res));
+
+        main.createChart(dataLists, "qpsk");
+//        main.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        main.setBounds(10, 10, 500, 500);
+//        main.setTitle("にゃあ");
+//        main.setVisible(true);
+        end = System.currentTimeMillis();
         System.out.println("time" + (end - start) + "ms");
     }
 
 
-    public void createChart(List<Double> value) {
-
-        JFreeChart freeChart = ChartFactory.createXYLineChart("BER", "SNR [dB]", "nyaa", createData(value), PlotOrientation.VERTICAL, false, false, false);
+    public void createChart(List<Double> value, String name) {
+        JFreeChart freeChart = ChartFactory.createXYLineChart(name, "SNR [dB]", "nyaa", createData(value), PlotOrientation.VERTICAL, false, false, false);
         ChartPanel cpanel = new ChartPanel(freeChart);
         getContentPane().add(cpanel, BorderLayout.CENTER);
         setLogAxis(freeChart);
         try {
-            ChartUtilities.saveChartAsJPEG(new File("chart.jpg"), freeChart, 500, 500);
+            ChartUtilities.saveChartAsJPEG(new File(name + ".jpg"), freeChart, 500, 500);
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
     static void setLogAxis(JFreeChart chart) {
-        LogAxis axis = new LogAxis("計算値");
+        LogAxis axis = new LogAxis("BER");
         axis.setLabelFont(new Font("SansSerif", Font.PLAIN, 16));
         axis.setRange(Math.pow(10, -7), Math.pow(10, 0));
         chart.getXYPlot().setRangeAxis(axis);
@@ -118,7 +132,7 @@ public class Main extends JFrame {
             double sigma = Math.sqrt(Math.pow(10, -(1.0 / 10) * sn));
             for (int i = 0; i < BIT_NUM; i++) {
                 double noise = new Random().nextGaussian() * sigma;
-                double fukutyo1 = (SK + noise) * 1.0 / (1 + Math.cos(4 * Math.PI * FC * 0));
+                double fukutyo1 = (BPSK + noise) * Math.cos(2 * Math.PI * FC * 0);
                 if (fukutyo1 > 0) error++;
                 res = error / BIT_NUM;
             }
@@ -138,9 +152,10 @@ public class Main extends JFrame {
             double sigma = Math.sqrt(Math.pow(10, -(1.0 / 10) * sn));
             for (int i = 0; i < BIT_NUM; i++) {
                 double noise = new Random().nextGaussian() * sigma;
-                double fukutyo1 = (SK + noise) * Math.cos(2 * Math.PI * FC * 0 + 0 * Math.PI);
+                double fukutyo1 = (QPSK + noise) * Math.cos(2 * Math.PI * FC * 0);
+                double fukutyo2 = (QPSK + noise) * Math.sin(2 * Math.PI * FC * 0);
 //                System.out.println(fukutyo1+"\t"+fukutyo2);
-                if (fukutyo1 > 0) error++;
+                if (fukutyo1 < 0 || fukutyo2 < 0) error++;
                 res = error / BIT_NUM;
             }
         }
