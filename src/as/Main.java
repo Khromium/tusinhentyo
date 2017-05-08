db60f1944c6198d0ccf3a4acfb7882d69ea258fb
khrom <devs@outlook.jp>
2017-05-09 00:44:13 +0900

    update: 日付が変わったので寝ます

--- src/as/Main.java
+++ src/as/Main.java
@@ -23,12 +23,32 @@ import java.util.List;
  * Created by khrom on 2017/04/22.
  */
 public class Main extends JFrame {
-    private static int SN_RANGE = 18;
-    private static double BIT_NUM = Math.pow(10, 7);
+    private static int SN_RANGE = 18;//計算するDB数。この数だけスレッド生成する
+    private static double BIT_NUM = Math.pow(10, 6);
     private static int FC = 50; //carrier freq
-    private static double BPSK = 1 * Math.cos(2 * Math.PI * FC * 0 + (1) * Math.PI);
-    private static double QPSK = 1 * Math.cos(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);
-    private static double QAM = 1 * Math.cos(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI) + 1 * Math.sin(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);
+    private static double BPSK = 1 * Math.cos(2 * Math.PI * FC * 0 + (1) * Math.PI);//r=1
+    private static double QPSK = 1 * Math.cos(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);//r=1,象限で区切るためにπ/4だけずらしている。
+    /**
+     * グラフ上で1がr=1となるように
+     *        Q
+     *  1     ┤
+     *1/√2    ┤   2     1
+     *        │
+     *1/(3√2) ┤   3     2
+     *        │
+     *        ┼───┬─────┬───┬──I
+     *      0  1/(3√2) 1/√2 1
+     *  2: r=√5/3
+     *  3: r=1/3
+     *  時間を進めないとsinが0になるので、1/4だけtを進める
+     *  参考: http://get-mobilebb.com/v001s010c026h0002r001.html
+     */
+    private static double QAM_1 = 1 * Math.cos(2 * Math.PI * FC * 1 / 4.0 + (1 / 4.0) * Math.PI)
+            + 1 * Math.sin(2 * Math.PI * FC * 1 / 4.0 + (1 / 4.0) * Math.PI);
+    private static double QAM_2 = Math.sqrt(5) / 3.0 * Math.cos(2 * Math.PI * FC * 1 / 4.0 + (2 / 5.0) * Math.PI)
+            + Math.sqrt(5) / 3.0 * Math.sin(2 * Math.PI * FC * 1 / 4.0 + (2 / 5.0) * Math.PI);
+    private static double QAM_3 = 1 / 3.0 * Math.cos(2 * Math.PI * FC * 1 / 4.0 + (1 / 4.0) * Math.PI)
+            + 1 / 3.0 * Math.sin(2 * Math.PI * FC * 1 / 4.0 + (1 / 4.0) * Math.PI);
 
 
     public static void main(String[] args) {
@@ -36,48 +56,48 @@ public class Main extends JFrame {
         long start = System.currentTimeMillis();
 
 
-        List<CalBPSK> calBPSKS = new ArrayList<>();
-        for (int i = 0; i < SN_RANGE; i++) {
-            calBPSKS.add(new CalBPSK(i));
-            calBPSKS.get(i).start();
-        }
-        calBPSKS.forEach(s -> {
-            try {
-                s.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        List<Double> dataList = new ArrayList<>();
-        calBPSKS.forEach(s -> dataList.add(s.res));
-
-        main.createChart(dataList, "BPSK");
+//        List<CalBPSK> calBPSKS = new ArrayList<>();
+//        for (int i = 0; i < SN_RANGE; i++) {
+//            calBPSKS.add(new CalBPSK(i));
+//            calBPSKS.get(i).start();
+//        }
+//        calBPSKS.forEach(s -> {
+//            try {
+//                s.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//
+//        List<Double> dataList = new ArrayList<>();
+//        calBPSKS.forEach(s -> dataList.add(s.res));
+//
+//        main.createChart(dataList, "BPSK");
         long end = System.currentTimeMillis();
-        System.out.println("time" + (end - start) + "ms");
+//        System.out.println("time" + (end - start) + "ms");
 
 ////        //qpsk
-        start = System.currentTimeMillis();
-
-        List<CalQPSK> calQPSKS = new ArrayList<>();
-        for (int i = 0; i < SN_RANGE; i++) {
-            calQPSKS.add(new CalQPSK(i));
-            calQPSKS.get(i).start();
-        }
-        calQPSKS.forEach(s -> {
-            try {
-                s.join();
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        });
-
-        List<Double> dataLists = new ArrayList<>();
-        calQPSKS.forEach(s -> dataLists.add(s.res));
-
-        main.createChart(dataLists, "QPSK");
-        end = System.currentTimeMillis();
-        System.out.println("time" + (end - start) + "ms");
+//        start = System.currentTimeMillis();
+//
+//        List<CalQPSK> calQPSKS = new ArrayList<>();
+//        for (int i = 0; i < SN_RANGE; i++) {
+//            calQPSKS.add(new CalQPSK(i));
+//            calQPSKS.get(i).start();
+//        }
+//        calQPSKS.forEach(s -> {
+//            try {
+//                s.join();
+//            } catch (InterruptedException e) {
+//                e.printStackTrace();
+//            }
+//        });
+//
+//        List<Double> dataLists = new ArrayList<>();
+//        calQPSKS.forEach(s -> dataLists.add(s.res));
+//
+//        main.createChart(dataLists, "QPSK");
+//        end = System.currentTimeMillis();
+//        System.out.println("time" + (end - start) + "ms");
 
 //        //16qam
         start = System.currentTimeMillis();
@@ -106,8 +126,9 @@ public class Main extends JFrame {
 
     /**
      * グラフ作成して画像を保存します。
+     *
      * @param value 計算データ
-     * @param name グラフ名
+     * @param name  グラフ名
      */
     public void createChart(List<Double> value, String name) {
         JFreeChart freeChart = ChartFactory.createXYLineChart(name, "SNR [dB]", "nyaa", createData(value), PlotOrientation.VERTICAL, false, false, false);
@@ -123,6 +144,7 @@ public class Main extends JFrame {
 
     /**
      * 対数軸を設定します。
+     *
      * @param chart
      */
     static void setLogAxis(JFreeChart chart) {
@@ -138,6 +160,7 @@ public class Main extends JFrame {
 
     /**
      * グラフに設置するためのデータ生成
+     *
      * @param value
      * @return
      */
@@ -188,9 +211,9 @@ public class Main extends JFrame {
             double sigma = Math.sqrt(Math.pow(10, -(1.0 / 10) * sn));
             for (int i = 0; i < BIT_NUM; i++) {
                 double noise = new Random().nextGaussian() * sigma;
-                double fukutyo1 = (QPSK + noise) * Math.cos(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);
+                double fukutyo1 = (QPSK + noise) * Math.cos(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);//象限で区切るためにπ/4だけずらしている。
                 noise = new Random().nextGaussian() * sigma;
-                double fukutyo2 = (QPSK + noise) * Math.sin(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);
+                double fukutyo2 = (QPSK + noise) * Math.sin(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);//象限で区切るためにπ/4だけずらしている。
                 if (fukutyo1 < 0 || fukutyo2 < 0) error++;
                 res = error / BIT_NUM;
             }
@@ -199,6 +222,7 @@ public class Main extends JFrame {
 
     /**
      * 16QAMの計算
+     * TODO: 複数ビット誤りを検出する
      */
     public static class CalQAM extends Thread {
         private double sn;
@@ -209,16 +233,62 @@ public class Main extends JFrame {
         }
 
         public void run() {
-            int error = 0;
+            int corner = 0, edge = 0, center = 0;
             double sigma = Math.sqrt(Math.pow(10, -(1.0 / 10) * sn));
-            for (int i = 0; i < BIT_NUM; i++) {
+
+            //角　ex.1000
+            //I:x軸、Q:Y軸 16の内4つ 4/16 = 1/4
+            for (int i = 0; i < BIT_NUM / 4; i++) {
                 double noise = new Random().nextGaussian() * sigma;
-                double fukutyo1 = (QAM + noise) * Math.cos(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);
+                double I = (QAM_1 + noise) * Math.cos(2 * Math.PI * FC * 1 / 4.0);//2nπ以外にしないと復調がうまくいかない
                 noise = new Random().nextGaussian() * sigma;
-                double fukutyo2 = (QAM + noise) * Math.sin(2 * Math.PI * FC * 0 + (1 / 4.0) * Math.PI);
-                if (fukutyo1 < 2 / 3.0 || fukutyo2 < 2 / 3.0) error++;
-                res = error / BIT_NUM;
+                double Q = (QAM_1 + noise) * Math.sin(2 * Math.PI * FC * 1 / 4.0);//2nπ以外にしないと復調がうまくいかない
+                //Iの誤り
+                if ((0 <= I && I < 2 / (3 * Math.sqrt(2))) || I < -2 / (3 * Math.sqrt(2))) corner += 1;//1ビット誤り
+                if (0 > I && I > -2 / (3 * Math.sqrt(2))) corner += 2;//2ビット誤り
+                //Qの誤り
+                if ((0 <= Q && Q < 2 / (3 * Math.sqrt(2))) || Q < -2 / (3 * Math.sqrt(2))) corner += 1;//1ビット誤り
+                if (0 > Q && Q > -2 / (3 * Math.sqrt(2))) corner += 2;//2ビット誤り
+//                res = error / BIT_NUM;
             }
+//            System.out.println("bitnum"+BIT_NUM/4+"¥ncorner:"+corner);
+
+            //中心 ex.1101
+            //I:x軸、Q:Y軸 16の内4つ 4/16 = 1/4
+            for (int i = 0; i < BIT_NUM / 4; i++) {
+                double noise = new Random().nextGaussian() * sigma;
+                double I = (QAM_2 + noise) * Math.cos(2 * Math.PI * FC * 1 / 4.0);//2nπ以外にしないと復調がうまくいかない
+                noise = new Random().nextGaussian() * sigma;
+                double Q = (QAM_2 + noise) * Math.sin(2 * Math.PI * FC * 1 / 4.0);//2nπ以外にしないと復調がうまくいかない
+                //Iの誤り
+                if ((0 > I && I < -2 / (3 * Math.sqrt(2))) || I > 2 / (3 * Math.sqrt(2))) center += 1;//1ビット誤り
+                if (I <= -2 / (3 * Math.sqrt(2))) center += 2;//2ビット誤り
+                //Qの誤り
+                if ((0 > Q && Q < -2 / (3 * Math.sqrt(2))) || Q > 2 / (3 * Math.sqrt(2))) center += 1;//1ビット誤り
+                if (Q <= -2 / (3 * Math.sqrt(2))) center += 2;//2ビット誤り
+//                res = error / BIT_NUM;
+            }
+//            System.out.println("bitnum"+BIT_NUM/4+"¥ncenter:"+center);
+
+            //隅 ex.1100
+            //I:x軸、Q:Y軸 16の内4つ 8/16 = 1/2
+            for (int i = 0; i < BIT_NUM / 2; i++) {
+                double noise = new Random().nextGaussian() * sigma;
+                double I = (QAM_3 + noise) * Math.cos(2 * Math.PI * FC * 1 / 4.0);//2nπ以外にしないと復調がうまくいかない
+                noise = new Random().nextGaussian() * sigma;
+                double Q = (QAM_3 + noise) * Math.sin(2 * Math.PI * FC * 1 / 4.0);//2nπ以外にしないと復調がうまくいかない
+                //Iの誤り
+                if ((0 > I && I < -2 / (3 * Math.sqrt(2))) || I > 2 / (3 * Math.sqrt(2))) edge += 1;//1ビット誤り
+                if (I <= -2 / (3 * Math.sqrt(2))) edge += 2;//2ビット誤り
+                //Qの誤り
+                if (0 <= Q && Q < 2 / (3 * Math.sqrt(2)) || Q < -2 / (3 * Math.sqrt(2))) edge += 1;//1ビット誤り
+                if (Q < 0 && Q >= -2 / (3 * Math.sqrt(2))) edge += 2;//2ビット誤り
+//                res = error / BIT_NUM;
+            }
+//            System.out.println("bitnum"+BIT_NUM/2+"¥nedge:"+edge);
+
+            res = (corner + center + edge) / (BIT_NUM * 4.0);         //角　ex.1000
+
         }
     }
 }
